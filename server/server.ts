879a106316927d021c3b81dabb76e0d7767593a8
ananteasytocheck <anant@easytocheck.com>
2026-06-30 21:12:46 +0530

    last_change

--- server/server.ts
+++ server/server.ts
@@ -39,7 +39,7 @@ await connectDB()
 // these cors only for express cors
 app.use(cors({
     // It means only these url's are allowed tp make request
-    origin:['http://localhost:5173','http://127.0.0.1:5173"','http://localhost:3000','https://gen-thumb-ai.vercel.app'],
+    origin:['http://localhost:5173','http://127.0.0.1:5173"','http://localhost:3000','https://gen-thumb-ai-client.vercel.app'],
     // It allows cookies/sessions
     credentials:true
 }))
