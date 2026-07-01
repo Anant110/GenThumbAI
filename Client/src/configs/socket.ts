aad5bcb10fa4feaba9058246043313ba3399df5a
ananteasytocheck <anant@easytocheck.com>
2026-07-01 20:07:54 +0530

    socket_change

--- Client/src/configs/socket.ts
+++ Client/src/configs/socket.ts
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 export const socket=io(
-    "http://localhost:3000",
+    "https://gen-thumb-serverupdated.vercel.app",
     {
         withCredentials:true
     }
